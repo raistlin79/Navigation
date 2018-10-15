21c285baf0341f8508be3be79b6fc96717cac33f
Sascha Krell <sascha.krell@googlemail.com>
2018-10-16 01:47:49 +0200

    Solution candidate I

--- my_dqn_agent.py
+++ my_dqn_agent.py
@@ -8,38 +8,40 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
-UPDATE_EVERY = 4        # how often to update the network
+BUFFER_SIZE = int(1e5)   # replay buffer size
+BATCH_SIZE = 64          # minibatch size
+GAMMA = 0.99             # discount factor
+TAU = 1e-3               # for soft update of target parameters
+LR = 5e-4                # learning rate
+UPDATE_EVERY = 4         # how often to update the network
+
+SEED = 0
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size):
         """Initialize an Agent object.
 
         Params
         ======
             state_size (int): dimension of each state
             action_size (int): dimension of each action
-            seed (int): random seed
         """
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(seed)
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_local = QNetwork(state_size=self.state_size, action_size=self.action_size, seed=SEED).to(device)
+        self.qnetwork_target = QNetwork(state_size=self.state_size, action_size=self.action_size, seed=SEED).to(device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, SEED)
+
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
 
@@ -87,12 +89,10 @@ class Agent():
         """
         states, actions, rewards, next_states, dones = experiences
 
-        # Get max predicted Q values (for next states) from target model
-        # TODO: Using Double DQN
-        # Q_actions_next = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
-        # Q_targets_next = self.qnetwork_target(next_states).detach().gather(1, Q_actions_next)
+        # Get max predicted Q values (for next states) from target model using Double DQN approach
+        Q_actions_next = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).detach().gather(1, Q_actions_next)
 
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
 
         # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
--- my_model.py
+++ my_model.py
@@ -2,35 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class QNetwork(nn.Module):
-    """Actor (Policy) Model."""
+    """Actor (Policy) Model. DuelingQNetwork"""
 
-    def __init__(self, state_size, action_size, seed, fc1_units, fc2_units):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-        """
-        super(QNetwork, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
-
-    def forward(self, state):
-        """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
-
-class DuelingQNetwork(nn.Module):
-    """Actor (Policy) Model."""
-
-    def __init__(self, state_size, action_size, seed, hidden_sizes=64, dueling_sizes=64):
+    def __init__(self, state_size, action_size, seed):
         """Initialize parameters and build model.
         Params
         ======
@@ -40,42 +16,36 @@ class DuelingQNetwork(nn.Module):
             hidden_sizes (int_array): list of sizes of hidden layers
             dueling_sizes (int_array): list of sizes of hidden layers for dueling streams
         """
-        super(DuelingQNetwork, self).__init__()
+        super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.action_size = action_size
-        self.num_hidden_layers = len(hidden_sizes)
-        self.num_dueling_layers = len(dueling_sizes)
-        self.fc1 = nn.Linear(state_size, hidden_sizes[0])
-        self.layers = nn.ModuleList([nn.Linear(hidden_sizes[i], hidden_sizes[i+1]) for i in range(len(hidden_sizes) - 1)])
-        if len(dueling_sizes):
-            self.adv_fc1 = nn.Linear(hidden_sizes[-1], dueling_sizes[0])
-            self.val_fc1 = nn.Linear(hidden_sizes[-1], dueling_sizes[0])
-            self.adv_layers = nn.ModuleList([nn.Linear(dueling_sizes[i], dueling_sizes[i+1]) for i in range(len(dueling_sizes) - 1)])
-            self.val_layers = nn.ModuleList([nn.Linear(dueling_sizes[i], dueling_sizes[i+1]) for i in range(len(dueling_sizes) - 1)])
-            self.adv_out = nn.Linear(dueling_sizes[-1], action_size)
-            self.val_out = nn.Linear(dueling_sizes[-1], 1)
-        else:
-            self.adv_out = nn.Linear(hidden_sizes[-1], action_size)
-            self.val_out = nn.Linear(hidden_sizes[-1], 1)
+
+        # Input layer
+        self.fc1 = nn.Linear(state_size, 64)
+        # create hidden layers according to HIDDEN_SIZES
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 64)
+
+        # create duelling layers according to DUELLING_SIZES
+        self.adv_fc1 = nn.Linear(64, 64)
+        self.val_fc1 = nn.Linear(64, 64)
+
+        # Output layer
+        self.adv_out = nn.Linear(64, action_size)
+        self.val_out = nn.Linear(64, 1)
+
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        for l in self.layers:
-            x = F.relu(l(x))
         adv, val = None, None
-        if self.num_dueling_layers:
-            adv = F.relu(self.adv_fc1(x))
-            val = F.relu(self.val_fc1(x))
-            for a_l in self.adv_layers:
-                adv = F.relu(a_l(adv))
-            for v_l in self.val_layers:
-                val = F.relu(v_l(val))
-            adv = self.adv_out(adv)
-            val = self.val_out(val)
-        else:
-            adv = self.adv_out(x)
-            val = self.val_out(x)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+
+        adv = F.relu(self.adv_fc1(x))
+        val = F.relu(self.val_fc1(x))
+        adv = self.adv_out(adv)
+        val = self.val_out(val).expand(x.size(0), self.action_size)
 
         x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.action_size)
 
